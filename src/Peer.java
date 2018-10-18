c3c51bce418bbb9008222749fb6b363e6564ba0a
raymondhujc@gmail.com <raymondhujc@gmail.com>
2018-10-18 11:34:07 -0400

    add config reader

--- src/Peer.java
+++ src/Peer.java
@@ -3,20 +3,29 @@ import configs.PeerInfo;
 
 import java.util.BitSet;
 import java.util.List;
+import java.util.Map;
 
 public class Peer {
     private PeerInfo peerInfo;
     private CommonInfo commonInfo;
-    private List<PeerInfo> peerList;
-
-
+    private Map<Integer, PeerInfo> peerMap;
 
     Peer(int peerId, CommonInfo commonInfo) {
         this.peerInfo = new PeerInfo(peerId);
         this.commonInfo = commonInfo;
+
+        // init bit field
+
     }
 
     public void start(List<PeerInfo> peerList) {
+        // load neighbor info
+        for(PeerInfo info : peerList) {
+            peerMap.put(info.getPeerId(), info);
+        }
+
+        // load this peer config
+        
 
     }
 }
--- src/configs/CommonInfo.java
+++ src/configs/CommonInfo.java
@@ -1,8 +1,5 @@
 package configs;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class CommonInfo {
 
     private int numberOfPreferredNeighbors;
@@ -11,41 +8,6 @@ public class CommonInfo {
     private String fileName;
     private int fileSize;
     private int pieceSize;
-
-    public CommonInfo(String commonConfigDir) {
-        String st;
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(commonConfigDir));
-            while ((st = in.readLine()) != null) {
-                String[] tokens = st.split("\\s+");
-                switch (tokens[0]) {
-                    case "NumberOfPreferredNeighbors":
-                        numberOfPreferredNeighbors = Integer.parseInt(tokens[1]);
-                        break;
-                    case "UnchokingInterval":
-                        unchokingInterval = Integer.parseInt(tokens[1]);
-                        break;
-                    case "OptimisticUnchokingInterval":
-                        optimisticUnchokingInterval = Integer.parseInt(tokens[1]);
-                        break;
-                    case "FileName":
-                        fileName = tokens[1];
-                        break;
-                    case "FileSize":
-                        fileSize = Integer.parseInt(tokens[1]);
-                        break;
-                    case "PieceSize":
-                        pieceSize = Integer.parseInt(tokens[1]);
-                        break;
-                    default:
-                        throw (new Exception("wrong common config entry: " + tokens[1]));
-                }
-            }
-            in.close();
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-    }
 
     public int getNumberOfPreferredNeighbors() {
         return numberOfPreferredNeighbors;
@@ -69,5 +31,29 @@ public class CommonInfo {
 
     public int getPieceSize() {
         return pieceSize;
+    }
+
+    public void setNumberOfPreferredNeighbors(int numberOfPreferredNeighbors) {
+        this.numberOfPreferredNeighbors = numberOfPreferredNeighbors;
+    }
+
+    public void setUnchokingInterval(int unchokingInterval) {
+        this.unchokingInterval = unchokingInterval;
+    }
+
+    public void setOptimisticUnchokingInterval(int optimisticUnchokingInterval) {
+        this.optimisticUnchokingInterval = optimisticUnchokingInterval;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public void setPieceSize(int pieceSize) {
+        this.pieceSize = pieceSize;
     }
 }
